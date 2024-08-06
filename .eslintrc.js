6e2599e7389ee2ddff1ed812b93cf133fc673d09
szilard_jung <jungszilard@gmail.com>
2024-08-06 14:50:18 +0200

    chore: add eslint + prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: "expo",
+  plugins: ["prettier"],
+  rules: {
+    "no-console": "warn",
+    "no-use-before-define": "error",
+    "no-unused-vars": "error",
+  },
+};
