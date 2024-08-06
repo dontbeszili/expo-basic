2a3f46d4002b8084bf0cb7345c3a6a5322f77f22
szilard_jung <jungszilard@gmail.com>
2024-08-06 21:37:47 +0200

    refactor: remove unused packages, images

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import "./globals.css";
-// import Logo from "../../assets/bcr.svg";
 
 export default function Index() {
   return (
     <View className='flex-1 justify-center items-center bg-primary'>
-      {/* <Logo width={100} height={100} /> Use the SVG */}
+      <Image source={require("@/assets/bcr.png")} className='mb-10' />
       <Text className='text-bcr font-bold text-xl'>Welcome</Text>
       <StatusBar style='auto' />
     </View>
